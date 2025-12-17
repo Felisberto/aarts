73824984c4a5f37eb6058e638e6b471c2a308335
Felipe Barros <90708370+Felisberto@users.noreply.github.com>
2025-12-17 10:42:16 -0300

    Adjust container padding in page.tsx
    Removed top padding from the container div.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <>
       <div className="flex items-center justify-center min-h-full">
-        <div className="container mx-auto px-4 md:px-8 pt-4 pb-8">
+        <div className="container mx-auto px-4 md:px-8 pb-8">
           <div className="mb-8 text-left">
             <h2 className="text-2xl font-bold font-headline text-primary tracking-tight">
               Momentos
